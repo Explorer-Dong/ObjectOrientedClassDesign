764fe4f8f2052dd0886677c940252c009d401c79
Mr_Dwj <explorer-dong@foxmail.com>
2023-09-06 22:19:52 +0800

    web03 编写后端视图函数，打好前端架构

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, request, render_template
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import text
 
 app = Flask(__name__)
 
@@ -9,15 +8,24 @@ app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://root:123456@localhost:3
 db = SQLAlchemy(app)
 
 
-with app.app_context():
-    with db.engine.connect() as con:
-        rs = con.execute(text("select 1"))
-        print(rs.fetchone())
-
-
 @app.route("/")
 def index():
     return render_template("index.html")
+
+
+@app.route("/course", methods=["GET", "POST"])
+def course():
+    return render_template("course.html")
+
+
+@app.route("/score", methods=["GET", "POST"])
+def score():
+    return render_template("score.html")
+
+
+@app.route("/student", methods=["GET", "POST"])
+def student():
+    return render_template("student.html")
 
 
 if __name__ == "__main__":
