181cebb2d60a0b5ae5e9b2033f31422bc50c05d6
Mr_Dwj <explorer-dong@foxmail.com>
2023-09-06 22:18:21 +0800

    web02 连接数据库

--- app.py
+++ app.py
@@ -1,6 +1,18 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import text
 
 app = Flask(__name__)
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://root:123456@localhost:3306/admin?charset=utf8mb4"
+
+db = SQLAlchemy(app)
+
+
+with app.app_context():
+    with db.engine.connect() as con:
+        rs = con.execute(text("select 1"))
+        print(rs.fetchone())
 
 
 @app.route("/")
