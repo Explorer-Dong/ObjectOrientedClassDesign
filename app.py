3ecfe661c6e236ee67aa0ccf36486abfe7f106a6
Mr_Dwj <explorer-dong@foxmail.com>
2023-09-06 22:22:30 +0800

    web06

--- app.py
+++ app.py
@@ -40,7 +40,7 @@ def index():
     return render_template("index.html")
 
 
-@app.route("/course", methods=["GET", "POST"])
+@app.route("/course/", methods=["GET", "POST"])
 def course():
     if request.method == "GET":
         courses = Course.query.all()
@@ -50,7 +50,7 @@ def course():
 
 
 # 分数表：主页 + 搜索
-@app.route("/score", methods=["GET", "POST"])
+@app.route("/score/", methods=["GET", "POST"])
 def score():
     if request.method == "GET":
         scores = Score.query.all()
@@ -63,9 +63,59 @@ def score():
         else:
             scores = Score.query.filter(Score.name.like("%" + name_keyword + "%")).all()
             return render_template("score.html", name_keyword=name_keyword, scores=scores)
+        
+
+# 分数表：增加
+@app.route("/score-add/", methods=["GET", "POST"])
+def score_add():
+    if request.method == "GET":
+        return render_template("score-add.html")
+    else:
+        number = request.form.get("number")
+        name = request.form.get("name")
+        chinese = request.form.get("chinese")
+        math = request.form.get("math")
+        english = request.form.get("english")
+        score = Score(number=number, name=name, chinese=chinese, math=math, english=english)
+        db.session.add(score)
+        db.session.commit()
+        return redirect("/score")
 
 
-@app.route("/student", methods=["GET", "POST"])
+# 分数表：删除，传入的参数是学号
+@app.route("/score-delete/<string:score_number>", methods=["GET", "POST"])
+def score_delete(score_number):
+    if request.method == "POST":
+        if request.form.get("confirmed") == "true":
+            score = Score.query.filter(Score.number == score_number).first()
+            db.session.delete(score)
+            db.session.commit()
+    return redirect("/score")
+
+
+# 分数表：修改，传入的参数是学号
+@app.route("/score-correct/<string:score_number>", methods=["GET", "POST"])
+def score_correct(score_number):
+    if request.method == "GET":
+        return render_template("score-correct.html")
+    else:
+        score = Score.query.filter(Score.number == score_number).first()
+
+        new_name = request.form.get("name")
+        new_chinese = request.form.get("chinese")
+        new_math = request.form.get("math")
+        new_english = request.form.get("english")
+
+        score.name = new_name
+        score.chinese = new_chinese
+        score.math = new_math
+        score.english = new_english
+
+        db.session.commit()
+        return redirect("/score")
+
+
+@app.route("/student/", methods=["GET", "POST"])
 def student():
     if request.method == "GET":
         students = Student.query.all()
@@ -75,4 +125,4 @@ def student():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, port=5001)
