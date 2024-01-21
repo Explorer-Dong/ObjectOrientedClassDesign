519c8fe9b4694cd99a853539d786a135a9861192
Mr_Dwj <explorer-dong@foxmail.com>
2024-01-21 15:44:47 +0800

    upd

--- config.py
+++ config.py
@@ -7,10 +7,10 @@ PASSWORD = '123456'
 DB_URI = 'mysql+pymysql://{}:{}@{}:{}/{}?charset=utf8mb4'.format(USERNAME,PASSWORD,HOSTNAME,PORT,DATABASE)
 SQLALCHEMY_DATABASE_URI = DB_URI
 
-# 邮箱的配置信息
+# 邮箱的配置信息（qq邮箱演示）
 MAIL_SERVER = 'smtp.qq.com'
 MAIL_USE_SSL = True
 MAIL_PORT = 465
-MAIL_USERNAME = '2307975182@qq.com'
-MAIL_PASSWORD = 'jnnffcevwozaebhf'
-MAIL_DEFAULT_SENDER = '2307975182@qq.com'
+MAIL_USERNAME = '...@qq.com'
+MAIL_PASSWORD = '...'
+MAIL_DEFAULT_SENDER = '...@qq.com'
